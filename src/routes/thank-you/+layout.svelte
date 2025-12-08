e6067098e29ba7530fabc9cc5ae2f426aa6f88c2
plloyd11 <plloyd11@gmail.com>
2025-12-08 09:32:38 -0500

    meta stuff

--- src/routes/thank-you/+layout.svelte
+++ src/routes/thank-you/+layout.svelte
@@ -7,6 +7,16 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<!-- Thank you page specific meta tags -->
+	<title>Thank you for subscribing | Hath</title>
+	<meta
+		name="description"
+		content="Thank you for subscribing to Hath. We will keep you posted on new releases, shows, and other news."
+	/>
+	<meta
+		name="keywords"
+		content="Hath, New Jersey Death Metal, merch, official, store, clothing, apparel, t-shirts, hoodies, accessories"
+	/>
 </svelte:head>
 
 <main class="thank-you-page h-screen bg-bg-dark">
